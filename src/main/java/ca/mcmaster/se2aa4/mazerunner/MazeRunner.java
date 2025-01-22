894fd9aa1e2985723b5c75c784ed92afb1a754cf
MarcoDava <marco.a.dava@gmail.com>
2025-01-21 22:17:58 -0500

    started working on mazerunner algorithm

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/MazeRunner.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/MazeRunner.java
@@ -7,16 +7,51 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class MazeRunner{
-    private String[] FactorizedPath = new Stack<>();
     private Stack<String> CanonizedPath = new Stack<>();
     private Stack<int[]> ForkLocations = new Stack<>();
     private int[] position = new int[2];//change at some point
     private int[] finishArea = new int[2];//change at some point
+    private String[][] mazeArray;
     private boolean facingNorth = false;
     private boolean facingSouth = false;
     private boolean facingEast = false;
     private boolean facingWest = true;
-    private Maze mazeMap = new Maze();
+
+    public MazeRunner(String[][] mazeArray, int[] entryPoint, int[] finishArea){
+        position = entryPoint;
+        this.finishArea = finishArea;
+        this.mazeArray=mazeArray;
+    }
+
+    public MazeRunnerAlgorithm(String[][] mazeArray){
+
+        while(!isFinish(position)){
+            if(isFork(position)){
+                ForkLocations.push(position);
+                if(!isWall(peekLeft())){
+                    turnLeft();
+                }
+                else if{
+                    moveForward();
+                }
+            }
+            if(isDeadend(position)){
+                backTrackToFork();
+                turnRight();
+            }
+            if(isWall(peekForward())){
+                if(isWall(peekLeft())){
+                    turnRight();
+                }
+                else{
+                    turnLeft();
+                }
+            }
+            else{
+                moveForward();
+            }
+        }
+    }
 
     public void addPath(String value){
         CanonizedPath.push(value);
@@ -31,10 +66,10 @@ public class MazeRunner{
             deletePath();
         }
         ForkLocations.pop();
-        turnLeft();
     }
 
-    public boolean isDeadend(String[][] mazeArray, int row,int col){
+    public boolean isDeadend(int[] position){
+        int row,col = position[0],position[1];
         int walls = 0;
         if(row>0){
             if(!isWall(mazeArray,row-1,col)){
@@ -64,7 +99,8 @@ public class MazeRunner{
         }
     }
 
-    public boolean isWall(String[][] mazeArray, int row,int col){
+    public boolean isWall(int[] position){
+        int row,col=position[0],position[1];
         if(mazeArray[row][col].equals("#")){
             return true;
         }
@@ -73,7 +109,8 @@ public class MazeRunner{
         }
     }
 
-    public boolean isFinish(String[][] mazeArray, int row,int col){
+    public boolean isFinish(int[] position){
+        int row,col=position[0],position[1];
         if(position==finishArea){
             return true;
         }
@@ -82,7 +119,18 @@ public class MazeRunner{
         }
     }
 
-    public boolean isFork(String[][] mazeArray, int row,int col){
+    public boolean isFork( int[] position){
+        int openPaths = amountOfPaths(position);
+        if(openPaths>2){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public int amountOfPaths(int[] position){
+        int row,col = position[0],position[1];
         int openPaths = 0;
         if(row>0){
             if(!isWall(mazeArray,row-1,col)){
@@ -104,11 +152,51 @@ public class MazeRunner{
                 openPaths++;
             }
         }
-        if(openPaths>2){
-            return true;
+        return openPaths;
+    }
+
+    public int[] peekForward(int[] position){
+        if(facingNorth){
+            return new int[]{position[0]+1,position[1]};
         }
-        else{
-            return false;
+        else if(facingSouth){
+            return new int[]{position[0]-1,position[1]};
+        }
+        else if(facingEast){
+            return new int[]{position[0],position[1]-1};
+        }
+        else if(facingWest){
+            return new int[]{position[0],position[1]+1};
+        }
+    }
+
+    public int[] peekLeft(int[] position){
+        if(facingNorth){
+            return new int[]{position[0],position[1]-1};
+        }
+        else if(facingSouth){
+            return new int[]{position[0],position[1]+1};
+        }
+        else if(facingEast){
+            return new int[]{position[0]+1,position[1]};
+        }
+        else if(facingWest){
+            return new int[]{position[0]-1,position[1]};
+        }
+    }
+
+    public int[] peekRight(int[] position){
+        if(facingNorth){
+            return new int[]{position[0],position[1]+1};
+        }
+        else if(facingSouth){
+            return new int[]{position[0],position[1]-1};
+        }
+        else if(facingEast){
+            return new int[]{position[0]-1,position[1]};
+        }
+        else if(facingWest){
+            return new int[]{position[0]+1,position[1]};
         }
     }
 
@@ -181,8 +269,10 @@ public class MazeRunner{
     }
 
     public String[] getFactorizedPath(){
+        cananonizedPathArray = getPath();
+        factorizedPath
         for(int i=0;i<CanonizedPath.size();i++){
-            FactorizedPath.push(CanonizedPath.pop());
+            
         }
     }
 }
